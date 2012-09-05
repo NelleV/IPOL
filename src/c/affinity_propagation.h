c2a12b924404b69ac046256a7d477e28ea208c7e
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2012-09-05 20:46:58 +0200

    FIX - now phi and tau are damped . Started implementing a nice C++ object version of the code, more flexible

--- src/c/affinity_propagation.h
+++ src/c/affinity_propagation.h
@@ -1,11 +1,37 @@
 #include <vector>
 
-void affinity_propagation(vector <double> image, unsigned int line,
-                unsigned int exemplar[],
-                  double lambda, int max_iter);
-
 void hierarchical_affinity_propagation(vector <double> image,
                 unsigned int line,
                 unsigned int n_layers,
                 unsigned int exemplar[],
                 double lambda, int max_iter);
+
+class HierarchicalAffinityPropagation{
+  public:
+      unsigned int n_layers;
+      unsigned int line;
+      double lambda;
+      unsigned int max_iter;
+      vector <double> similarities;
+      vector <unsigned int> exemplar;
+      vector <vector <double> > tau;
+      vector <vector <double> > phi;
+      vector <vector <double> > responsabilities;
+      vector <vector <double> > availabilities;
+      vector <vector <double> > preferences;
+      unsigned int length;
+
+      HierarchicalAffinityPropagation(vector <double> similarities,
+                                      unsigned int length,
+                                      unsigned int n_layers,
+                                      vector <unsigned int> exemplar,
+                                      double lambda,
+                                      unsigned int max_iter);
+
+  private:
+    void update_responsabilities(unsigned int l);
+    void update_availabilities(unsigned int l);
+    void compute_preferences();
+    void update_tau();
+    void update_phi();
+};
